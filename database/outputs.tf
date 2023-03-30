4d52cbbae3f7d1c6b915f9b7f774dd8aa4c02a96
Keenan <jonathan@wantsmore.coffee>
2023-03-30 10:12:52 -0400

    Update provider version, add outputs

--- database/outputs.tf
+++ database/outputs.tf
@@ -1,14 +1,24 @@
-output "db_name" {
+output "database_name" {
   value       = aws_db_instance.database.db_name
   description = "Value of the database name"
 }
 
-output "database_sg" {
+output "database_security_group_id" {
   value       = aws_security_group.database.id
   description = "Value of the database security group ID"
 }
 
-output "db_identifier" {
+output "database_identifier" {
   value       = aws_db_instance.database.identifier
   description = "Value of the database identifier"
+}
+
+output "database_host" {
+  value       = aws_db_instance.database.address
+  description = "Value of the database host"
+}
+
+output "database_port" {
+  value       = aws_db_instance.database.port
+  description = "Value of the database port"
 }
